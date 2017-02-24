7383bf16c7527531dc1d439bf9ebb4553a7a2761
Sanjay Mali <sanjay.mali@propstack.com>
2017-02-24 12:32:42 +0530

    inital commit

--- SlideMenu/ViewController.swift
+++ SlideMenu/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var trailingConstraint: NSLayoutConstraint!
+    var menuShowing = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.mainView.layer.shadowOpacity = 1
+        self.mainView.layer.shadowRadius = 5
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showMenu(_ sender: Any) {
+        if (menuShowing){
+            leadingConstraint.constant = 0
+            trailingConstraint.constant = 0
+        }else {
+            // Leading will be + value
+            leadingConstraint.constant = 150
+            //Trailing will be negative add - value
+            trailingConstraint.constant = -150
+            
+        }
+        UIView.animate(withDuration: 0.5, animations: {
+            //This will reposition everything
+            self.view.layoutIfNeeded()
+        })
+        menuShowing = !menuShowing
     }
-
-
+    
 }
 
